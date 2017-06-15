ff19e7c5506ee9f161a0d30f59da071dd4f4f7e2
jasonh9 <jhuang9@mail.sfsu.edu>
2017-06-14 20:57:38 -0700

    added readBot

---
+++ readBot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import praw
+
+reddit = praw.Reddit('defBot') # name is relative to the name within the praw.ini file
+
+subreddit = reddit.subreddit("dogpictures") # subreddit to monitor
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
