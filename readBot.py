82777b5d70559ff26f38e33771ac65f678f0bfb0
jasonh9 <jhuang9@mail.sfsu.edu>
2017-06-19 16:14:49 -0700

    install textblob as the text analysis

--- readBot.py
+++ readBot.py
@@ -4,7 +4,7 @@ import praw
 
 reddit = praw.Reddit('defBot') # name is relative to the name within the praw.ini file
 
-subreddit = reddit.subreddit("dogpictures") # subreddit to monitor
+subreddit = reddit.subreddit("worldnews") # subreddit to monitor
 
 for submission in subreddit.hot(limit=5):
     print("Title: ", submission.title)
