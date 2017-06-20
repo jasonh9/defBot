f33dbbdc07091bfd7f1cbe28a1b652df856163e7
jasonh9 <jhuang9@mail.sfsu.edu>
2017-06-20 15:33:43 -0700

    added reddit class

--- readBot.py
+++ readBot.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 
 import praw
+import re
+from textblob import TextBlob
 
-reddit = praw.Reddit('defBot') # name is relative to the name within the praw.ini file
 
-subreddit = reddit.subreddit("worldnews") # subreddit to monitor
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
+class redditClient(object):
+
+    '''
+    Generic reddit class for sentiment analysis
+    '''
+
+    reddit = praw.Reddit('defbotboot')
+    subreddit = reddit.subreddit("dogpictures")
+
+    def get_submissions(self):
+        for submission in self.subreddit.hot(limit=5):
+            print(submission.title)
+    
+def main():
+
+    reddit = redditClient()
+    reddit.get_submissions()
+
+if __name__ == "__main__":
+    # call the main function
+    main()
