3ed2d6d48c12821f7958150225a3b5865257ee46
jasonh9 <jhuang9@mail.sfsu.edu>
2017-06-26 16:29:20 -0700

    fixed reply.py, debugging function

--- reply.py
+++ reply.py
@@ -25,21 +25,22 @@ class redditReply(object):
                 # if file contains empty elements, remove them
                 reply_post = list(filter(None, reply_post))
         
-            for submission in subreddit.comments(limit = limit):
-                if submission.id not in reply_post:
-                    if re.search("!defbot rate comments", submission.body, re.IGNORECASE):
-                        # reply to the post
-                        submission.reply("Insert Sentiment Analysis here")
-                        print("Bot replying to : ", submission.body)
+        for submission in self.subreddit.comments(limit = limit):
+            if submission.id not in reply_post:
+                if re.search("!defbot rate comments", submission.body, re.IGNORECASE):
+                    # reply to the post
+                    # submission.reply("Insert Sentiment Analysis here")
+                    print(submission.id)
+                    print("Bot replying to : ", submission.body)
         
-                        reply_post.append(submission.id)
+                    reply_post.append(submission.id)
         with open("reply_post.txt", "w") as f:
             for post_id in reply_post:
                 f.write(post_id + "\n")
 
 def main():
     reddit = redditReply()
-    reply = reddit.reply_comment(limit = 25)
+    reddit.reply_comment(limit = 25)
 
 
 if __name__ == "__main__":
