e7bcf221b82ca2b0f5d214a64a75cde86012dfae
jasonh9 <jhuang9@mail.sfsu.edu>
2017-08-03 17:26:29 -0700

    added setup.py dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,15 @@
 import sys
 from setuptools import setup
 
+setup(name='defbot',
+        version='0.1',
+        description='automated sentiment analysis bot for reddit',
+        url='https://github.com/jasonh9/defBot',
+        author='Jason Huang',
+        author_email='jhuang9@mail.sfsu.edu',
+        license='MIT',
+        packages=['defbot'],
+        zip_safe=False)
 
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
